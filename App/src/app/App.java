9ba657b1ac85ad4304be08323d0430e312d5b18e
Devin Booker <dbooker@basecampcodingacademy.org>
2020-02-05 08:50:49 -0600

    Added loading and saving methods

--- App/src/app/App.java
+++ App/src/app/App.java
@@ -1,7 +1,34 @@
 package app;
 
+import java.util.*;
+import java.io.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        ArrayList<VolunteerSheet> volunteers = loadVolunteers();
         System.out.println("Hello Java");
+    }
+
+    private static void saveVolunteers(ArrayList<VolunteerSheet> volunteers) {
+        try {
+            FileOutputStream fileStream = new FileOutputStream("orders.ser");
+            ObjectOutputStream os = new ObjectOutputStream(fileStream);
+            os.writeObject(volunteers);
+            os.close();
+        } catch (IOException ex) {
+            System.out.println("Failed to save volunteers.");
+        }
+    }
+
+    public static ArrayList<VolunteerSheet> loadVolunteers() {
+        try {
+            FileInputStream fileStream = new FileInputStream("orders.ser");
+            ObjectInputStream os = new ObjectInputStream(fileStream);
+            ArrayList<VolunteerSheet> volunteers = (ArrayList<VolunteerSheet>) os.readObject();
+            os.close();
+            return volunteers;
+        } catch (IOException | ClassNotFoundException ex) {
+            return new ArrayList<VolunteerSheet>();
+        }
     }
 }
